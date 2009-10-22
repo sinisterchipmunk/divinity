6452650864bb5cc62d8633ce8d0816f25be899cc
sinisterchipmunk <sinisterchipmunk@gmail.com>
2009-10-22 18:19:09 -0400

    Minor updates, no real progress

--- lib/open_gl/octree.rb
+++ lib/open_gl/octree.rb
@@ -1,46 +1,91 @@
 class OpenGl::Octree
-  def initialize()
-    
+  MAX_OCTREE_DEPTH = 8
+
+  # When the octree has this many objects in it or more, and its depth is less than MAX_OCTREE_DEPTH, it will attempt
+  # to subdivide itself and pass the objects off to its respective children.
+  THRESHOLD = 16
+  
+  attr_reader :objects, :bounding_boxes, :depth, :children, :parent
+
+  def initialize(depth = 0, parent = nil)
+    @parent = parent
+    @objects = []
+    @bounding_boxes = []
+    @children = []
+  end
+
+  # Validates this octree, building children, moving objects to siblings, and/or resizing itself as necessary.
+  def validate!
+    subdivide! if children.empty? and objects.length > THRESHOLD and depth < MAX_OCTREE_DEPTH
+    delegate_objects! unless objects.empty?
+    children.each { |c| c.validate! }
+    update_boundary!
+    @valid = true
+  end
+
+  # Marks this octree as invalid, so that it will be validated the next time it is tested.
+  # IMPORTANT: Validating the octree is expensive, and should not be done unless registered objects have changed.
+  def invalidate!
+    @valid = false
+  end
+
+  # Returns true if this octree is valid; false if it needs to be validated and potentially resized or subdivided.
+  def valid?
+    @valid
   end
 
   # Returns true if this octree lies completely within the visible frustum. If true, then all sublevel tests can
   # be safely skipped and objects rendered, because any objects in this octree must be within the visible frustum.
   def completely_visible?
-    
+    validate! unless valid?
   end
 
   # Returns true if this octree lies completely beyond the visible frustum. If true, then all sublevel tests can
   # be safely skipped and no objects rendered, because any objects in this octree must be outside the visible frustum.
   def completely_hidden?
-    
+    validate! unless valid?
   end
 
-  # The objects to be added can be of any type, but must respond to #position, #up, #view, #render! and #size. #size
-  # must return either a number (to be treated as a cube of equal proportions) or a Vector3d (to be treated as a cube
-  # of differing width, depth and height); #position must return a Vertex3d; #up and #view must return Vector3d's.
+  # The objects to be added can be of any type, but must respond to #position, #up, #view, #right, #render! and #size.
+  #
+  # #size must return either a number (to be treated as a cube of equal proportions) or a Vector3d (to be treated as a
+  # box of differing width, depth and height);
+  #
+  # #position, #up, #right and #view must return either Vertex3d or Vector3d.
+  #
   # See OpenGl::Camera for explanations of what these vectors are.
   #
-  # HOW I EXPECT THIS TO WORK:
-  # Each object should have the above direction vectors, or more ideally a Camera object that represents the object's
-  # direction (and delegation of those vector methods into Camera). With those vectors, size of cube seems (at this
-  # early stage) to be a simple process:
-  #   1. Get the right, view, and up vectors. They should already be normalized.
-  #   2. Create a negative copy of each so that we have left, rear and down vectors.
-  #   3. Get the width, height and depth W, H and D of the object. Divide each of these by 2.
-  #   4. Multiplying the resultant scalars against the vectors above should give points at the extents of each vector
-  #      directly relative to the provided sizes, in the object's local coordinates.
-  #   5. The resultant view vector will respresent the furthest extent forward this object is expected to reach; using
-  #      that as the origin, translate along the left vector by w/2 units until the furthest extent left is reached;
-  #      this is the front-left corner. Translate along the up and down vectors for front-left-top and
-  #      front-left-bottom. Repeat this process for front-right, rear-left and rear-right for a total of 8 points.
-  #   6. If any of these 8 points are visible, return true. Otherwise, return false.
   def add(*objects)
+    self.objects.concat objects
+    self.bounding_boxes.concat objects.collect { |obj| generate_bounding_box_for obj }
+    invalidate!
+    self
+  end
+
+  alias << add
+
+  protected
+  # The last step in self#validate!, this method will check the boundaries of this Octree against the positions and
+  # edges of each object and each child octree, resizing itself where necessary. If resizing would overstep the bounds
+  # of a sibling octree, then resizing does not happen (an octree can't share space with a sibling).
+  def update_boundary!
+
+  end
+
+  # self#validate! calls this when # of objects exceeds threshold AND depth < MAX_OCTREE_DEPTH AND children.empty?
+  def subdivide!
+    # need to create 8 children and position them where needed
+  end
+
+  # Called by self#validate! after subdividing (where necessary), unless objects.empty?
+  def delegate_objects!
     
   end
 
-  # see #add
-  def <<(*objects)
-    add *objects
-    self
+  # This is likely a CPU-intensive method and should not be called every frame.
+  def generate_bounding_box_for(obj)
+    size = obj.size
+    size = Vertex3d.new(size) unless size.kind_of? Vertex3d
+    frustum.bounding_box(size, obj.position, obj.view, obj.up, obj.right)
   end
 end
