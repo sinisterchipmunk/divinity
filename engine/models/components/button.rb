2b904565bc8ec52215acb74a21fc1c06a96c0074
sinisterchipmunk <sinisterchipmunk@gmail.com>
2009-12-03 19:45:18 -0500

    Generic events now include the model they were fired upon.
    Also, controllers pass their request arguments into the model during instantiation (but not options or blocks).

--- engine/models/components/button.rb
+++ engine/models/components/button.rb
@@ -4,7 +4,18 @@ class Components::Button < Engine::Model::Base
   attr_accessor :state
   attr_accessor :caption
 
-  def initialize
-    @caption = "Untitled"
+  # An arbitrary "action" that can be associated with this Button. It doesn't really do anything except
+  # make it easier to check which button you're working with without having to check the caption. The
+  # default value is equal to whatever the #caption is, except that it's underscored (ie "Single Player"
+  # becomes "single_player").
+  #
+  # Actually, any value can be assigned to the action. For instance, an action could be a proc so that
+  # you can pass around a small set of instructions. Of course, you need to call that proc manually.
+  #
+  attr_accessor :action
+
+  def initialize(caption = "Untitled", action = caption.underscore.tr(' ', '_'))
+    @caption = caption
+    @action = action
   end
 end
