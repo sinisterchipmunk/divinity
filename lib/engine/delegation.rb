8e7c0f743e9b96c3c1567074cc0b4b9a8174c619
sinisterchipmunk <sinisterchipmunk@gmail.com>
2009-12-09 13:43:17 -0500

    Cleaning out obsolete files

--- lib/engine/delegation.rb
+++ lib/engine/delegation.rb
@@ -4,24 +4,25 @@ module Engine::Delegation
            :lock_up_vector?, :lock_up_vector!, :lock_y_axis?, :lock_y_axis!, :rotate_view!,
            :point_visible?, :cube_visible?, :sphere_visible?, :rotate_view!, :to => :camera
 
-  def write(x, y, text)
+  def write(x, y, text, options = {})
     w = self.width
     h = self.height
+    font = Textures::Font.select(options)
 
     case x
       when :west, :left  then x = 0
-      when :east, :right then x = w - Textures::Font.select.text_width(text)
+      when :east, :right then x = w - font.text_width(text)
       else raise "X coordinate must be an integer or one of [:east, :west, :right, :left]" unless x.kind_of? Fixnum
     end
 
     case y
       when :north, :top    then y = 0
-      when :south, :bottom then y = h - Textures::Font.select.line_height
+      when :south, :bottom then y = h - font.line_height
       else raise "X coordinate must be an integer or one of [:north, :south, :top, :bottom]" unless x.kind_of? Fixnum
     end
 
     ortho(w, h) do
-      Textures::Font.select.put(x, y, text)
+      font.put(x, y, text)
     end
   end
 end
--- lib/interface/containers/panel.rb
+++
@@ -1,9 +0,0 @@
-class Interface::Containers::Panel < Interface::Components::Component
-  include Interface::Containers::Container
-
-  def initialize(layout=Interface::Layouts::FlowLayout.new, &blk)
-    super()
-    self.layout = layout
-    self.background_visible = false
-  end
-end
